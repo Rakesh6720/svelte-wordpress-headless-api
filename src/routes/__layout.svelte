e6935feeb2c0df960d419155a6544a91a0b14c15
Rakesh Baruah <rbaruah@gmail.com>
2022-04-10 18:01:45 -0500

    api endpoints added w/ dynamic routes

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<div class="wrapper"><slot /></div>
+
+<style>
+	.wrapper {
+		margin: 0 auto;
+		width: 50vw;
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+	}
+</style>
